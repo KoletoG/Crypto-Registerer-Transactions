b89e7b54ca049c72ffc6b9e8ebda6b238b831bbf
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-03 15:49:02 +0300

    refactor: Moved logic from constructor to a separate method
    Created BuildService() for cleaner code.

--- Crypto Registerer Transactions/Program.cs
+++ Crypto Registerer Transactions/Program.cs
@@ -13,11 +13,15 @@ namespace Crypto_Registerer_Transactions
         private ILogger<Program> _logger;
         public Program()
         {
+            _serviceProvider = BuildService();
+        }
+        private IServiceProvider BuildService()
+        {
             Log.Logger = new LoggerConfiguration().WriteTo.File(@"log.txt").CreateLogger();
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddSingleton<ILogicService, LogicService>();
-            serviceCollection.AddLogging(x => x.AddSerilog());
-            _serviceProvider = serviceCollection.BuildServiceProvider();
+            return new ServiceCollection()
+                .AddSingleton<ILogicService, LogicService>()
+                .AddLogging(x => x.AddSerilog())
+                .BuildServiceProvider();
         }
         static void Main(string[] args)
         {
@@ -26,52 +30,60 @@ namespace Crypto_Registerer_Transactions
         }
         private void Run()
         {
-            _logicService = _serviceProvider.GetRequiredService<ILogicService>();
-            _logger = _serviceProvider.GetRequiredService<ILogger<Program>>();
-            if (!File.Exists(@"..\..\wallets.text"))
+            try
             {
-                using (StreamWriter streamWriter = new StreamWriter(@"..\..\wallets.txt", true))
+                _logicService = _serviceProvider.GetRequiredService<ILogicService>();
+                _logger = _serviceProvider.GetRequiredService<ILogger<Program>>();
+                if (!File.Exists(@"..\..\wallets.text"))
                 {
-                    Console.WriteLine("Wallets.txt created successfully.");
-                }
-            }
-            Console.WriteLine("If you want to stop the application, either just press ENTER or just exit from the X");
-            while (!token.IsCancellationRequested)
-            {
-                Console.WriteLine("Enter wallet address:");
-                string wallet = Console.ReadLine() ?? string.Empty;
-                _logicService.IsExit(wallet, token);
-                if (_logicService.IsWalletExists(wallet))
-                {
-                    Console.WriteLine($"Wallet exists with transaction sum of {_logicService.SumOfTransactionsByWallet(wallet)}, do you want to register another transaction to this wallet? - Y / N");
-                    string response = Console.ReadLine() ?? string.Empty;
-                    _logicService.IsExit(response, token);
-                    if (_logicService.IsResponseY(response))
+                    using (StreamWriter streamWriter = new StreamWriter(@"..\..\wallets.txt", true))
                     {
-                        _logicService.SaveTransaction(wallet);
+                        Console.WriteLine("Wallets.txt created successfully.");
+                    }
+                }
+                Console.WriteLine("If you want to stop the application, either just press ENTER or just exit from the X");
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Enter wallet address:");
+                    string wallet = Console.ReadLine() ?? string.Empty;
+                    _logicService.IsExit(wallet, token);
+                    if (_logicService.IsWalletExists(wallet))
+                    {
+                        Console.WriteLine($"Wallet exists with transaction sum of {_logicService.SumOfTransactionsByWallet(wallet)}, do you want to register another transaction to this wallet? - Y / N");
+                        string response = Console.ReadLine() ?? string.Empty;
+                        _logicService.IsExit(response, token);
+                        if (_logicService.IsResponseY(response))
+                        {
+                            _logicService.SaveTransaction(wallet);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Transaction registration declined.");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Transaction registration declined.");
+                        Console.WriteLine("Wallet hasn't been registered, do you want to register a transaction? - Y / N");
+                        string response = Console.ReadLine() ?? string.Empty;
+                        _logicService.IsExit(response, token);
+                        if (_logicService.IsResponseY(response))
+                        {
+                            _logicService.SaveTransaction(wallet);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Transaction registration declined.");
+                        }
                     }
                 }
-                else
-                {
-                    Console.WriteLine("Wallet hasn't been registered, do you want to register a transaction? - Y / N");
-                    string response = Console.ReadLine() ?? string.Empty;
-                    _logicService.IsExit(response, token);
-                    if (_logicService.IsResponseY(response))
-                    {
-                        _logicService.SaveTransaction(wallet);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Transaction registration declined.");
-                    }
-                }
+                Console.WriteLine("App has stopped");
             }
-            Console.WriteLine("App has stopped");
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error came from {nameof(Run)}");
+                throw new Exception(ex.ToString());
+            }
         }
-        
+
     }
 }
