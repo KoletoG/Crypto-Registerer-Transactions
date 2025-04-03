8b9392df348ea1583f07c342224687a3ff2b40a6
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-03 10:15:04 +0300

    Add project files.

---
+++ Crypto Registerer Transactions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Crypto_Registerer_Transactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter wallet address:");
+        }
+    }
+}
