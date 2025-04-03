84e43ee36b2b1095455ac042416698d8541c8434
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-03 10:29:11 +0300

    feat: Added basic structure of the program
    Added the basic structure of the code for the program.

--- Crypto Registerer Transactions/Program.cs
+++ Crypto Registerer Transactions/Program.cs
@@ -4,7 +4,75 @@
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            if (!File.Exists(@"..\..\wallets.text")) 
+            {
+                using (StreamWriter streamWriter = new StreamWriter(@"..\..\wallets.txt", true))
+                {
+                    Console.WriteLine("Wallets.txt created successfully.");
+                }
+            }
             Console.WriteLine("Enter wallet address:");
+            string wallet = Console.ReadLine();
+            if (program.IsWalletExists(wallet))
+            {
+                int wholeSum = 0;
+                using (StreamReader streamReader = new StreamReader(@"..\..\wallets.txt"))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        if(streamReader.ReadLine() == wallet)
+                        {
+                            wholeSum += int.Parse(streamReader.ReadLine());
+                        }
+                    }
+                }
+                Console.WriteLine($"Wallet exists with transaction sum of {wholeSum}, do you want to register another transaction to this wallet? - Y / N"); 
+                if (Console.ReadLine() == "Y")
+                {
+                    Console.WriteLine("Enter sum to register:");
+                    int sum = int.Parse(Console.ReadLine());
+                    using (StreamWriter strW = new StreamWriter(@"..\..\wallets.txt", true))
+                    {
+                        strW.WriteLine(wallet);
+                        strW.WriteLine(sum);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter a wallet adress:");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wallet hasn't been registered, do you want to register a transaction? - Y / N");
+                if (Console.ReadLine() == "Y")
+                {
+                    Console.WriteLine("Enter sum to register:");
+                    int sum = int.Parse(Console.ReadLine());
+                    using (StreamWriter strW = new StreamWriter(@"..\..\wallets.txt", true))
+                    {
+                        strW.WriteLine(wallet);
+                        strW.WriteLine(sum);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter a wallet address:");
+                }
+            }
+        }
+        public bool IsWalletExists(string wallet)
+        {
+            try
+            {
+                return File.ReadLines(@"..\..\wallets.txt").Contains(wallet);
+            }
+            catch (Exception ex)
+            {
+                // _logger.LogError(ex, "Error in {MethodName}", nameof(IsWalletExists));
+                throw new Exception(ex.ToString());
+            }
         }
     }
 }
